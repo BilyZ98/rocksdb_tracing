74078003dce7fb4afa1c846c6bd0ae0e5e78ebb6
BilyZ98 <zhuangzhutao@gmail.com>
2022-07-22 23:25:43 +0800

    add 7-22

---
+++ trace_example/block_trace.sh
@@ -0,0 +1,5 @@
+
+
+../build/block_cache_trace_analyzer_tool \
+-block_cache_trace_path=/tmp/binary_trace_test_example \
+-human_readable_trace_file_path=/tmp/human_readable_block_trace_test_example
