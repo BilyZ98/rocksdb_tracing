2014533769022b1da72fa9b25be2171ee81e94a5
bilyz <zhuangzhutao@gmail.com>
2022-08-05 09:12:37 +0800

    add 8-4

--- trace_example/bench_trace_run.sh
+++ trace_example/bench_trace_run.sh
@@ -19,7 +19,7 @@ $exec_path   --benchmarks="fillseq" --db=$db_path \
 --key_size=20 --prefix_size=20 --keys_per_prefix=0 --value_size=100 \
 --cache_index_and_filter_blocks --cache_size=1048576 \
 --disable_auto_compactions=1 --disable_wal=1  \
---min_level_to_compress=-1  --num=1000000
+--min_level_to_compress=-1  --num=100000
 
 
 trace_data_dir="./trace_data_dir"
@@ -36,7 +36,7 @@ $exec_path --benchmarks="readrandom" --use_existing_db --db=$db_path --duration=
 --key_size=20 --prefix_size=20 --keys_per_prefix=0 --value_size=100 \
 --cache_index_and_filter_blocks --cache_size=1048576 \
 --disable_auto_compactions=1 --disable_wal=1 \
---min_level_to_compress=-1  --num=1000000 \
+--min_level_to_compress=-1  --num=100000 \
 --threads=16 \
 -block_cache_trace_file="$block_trace_file_path" \
 -block_cache_trace_max_trace_file_size_in_bytes=1073741824 \
@@ -47,9 +47,10 @@ $exec_path --benchmarks="readrandom" --use_existing_db --db=$db_path --duration=
 
 if [ -z $disable_bcp ]; then
 bct_path="../build/block_cache_trace_analyzer_tool"
+bct_human_file_path="$trace_data_dir/bct_human_readable_file"
 $bct_path \
--block_cache_trace_path=/tmp/binary_trace_test_example \
--human_readable_trace_file_path=/tmp/human_readable_block_trace_test_example
+ -block_cache_trace_path="$block_trace_file_path"\
+ -human_readable_trace_file_path="$bct_human_file_path"
 fi
 
 
@@ -70,8 +71,7 @@ $trace_analyzer_exec \
   -trace_path="$op_trace_file_path"
 
 
-io_tarce_parser_exec="../build/io_tracer_parser"
+io_trace_parser_exec="../build/io_tracer_parser"
 io_parser_res="$trace_data_dir/io_trace_res.log"
-$io_trace_parser_exec \
-  -io_trace_file="$io_trace_file_path" >  "$io_parser_res"
+$io_trace_parser_exec -io_trace_file="$io_trace_file_path" >  "$io_parser_res"
 
---
+++ trace_example/trace_dat_process.sh
@@ -0,0 +1,11 @@
+
+# first we will plot the simplest plot for now, then we will put 3 level 
+# trace info into one figure so that we can get some insight.
+
+
+# op level plot 
+# what data should we plot ? 
+# latency ? 
+# key size 
+# 
+dat_file=''
