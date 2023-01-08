4e1c27729e9e3abe1d93207d6f85e3494c857040
Ubuntu <azureuser@TestLinux.jjibom3ntijurou1ujwf2gkksb.xx.internal.cloudapp.net>
2023-01-08 07:49:55 +0000

    update sh file path

--- compact_block_cache_trace/get_compaction_block_cache_hit_rate.sh
+++ compact_block_cache_trace/get_compaction_block_cache_hit_rate.sh
@@ -2,8 +2,8 @@
 
 
 
-# in_file=/tmp/block_trace_human_file
-in_file=/tmp/standard_block_cache_trace_human_file
+in_file=/tmp/block_trace_human_file
+# in_file=/tmp/standard_block_cache_trace_human_file
 
 
 awk -F ',' '{print $9,$14}' $in_file | sort | uniq -c 
